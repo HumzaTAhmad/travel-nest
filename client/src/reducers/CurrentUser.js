cdfa39af0896d6c130ac274cdecf987867cc773e
HumzaTAhmad <humza3173@gmail.com>
2023-01-27 20:47:39 -0500

    Create reducer with update condition for current user
    Created this reducer so the current users information can be changed from
    within a component

---
+++ client/src/reducers/CurrentUser.js
@@ -0,0 +1,10 @@
+//state is the state of the current user
+export default (state = [], action) => {
+    switch(action.type){
+        case 'UPDATE_USER':
+            return {...state, currentUser:action.payload}
+        
+        default:
+            throw new Error('No matched action!')
+    }
+}
