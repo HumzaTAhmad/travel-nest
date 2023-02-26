d129e4b6728624d283f14a57f9bca29960c37f92
HumzaTAhmad <humza3173@gmail.com>
2023-02-26 13:55:24 -0500

    Handle the state of the submit button showing when all three steps of add room are completed
    This was done so the user cannot prematurely add a room into the server and display it before
    they finish all details of the room. If one of the steps isn't complete the submit button will not
    appear and the room may not be added. However, if they have all three complete they may add the room
    *Submit functionality is in progress*

--- client/src/components/addRoom/AddRoom.js
+++ client/src/components/addRoom/AddRoom.js
@@ -5,6 +5,7 @@ import AddDetails from './addDetails/AddDetails'
 import AddImages from './addImages/AddImages'
 import AddLocation from './addLocation/AddLocation'
 import {connect} from 'react-redux'
+import { Send } from '@mui/icons-material'
 
 function AddRoom(props) {
     const {images, details, location} = props
@@ -14,6 +15,8 @@ function AddRoom(props) {
         {label:'Details', completed:false},
         {label:'Images', completed:false}
     ])
+
+    const [showSubmit, setShowSubmit] = useState(false)
 
     const handleNext = () => {
         if (activeStep < steps.length - 1) {
@@ -66,6 +69,17 @@ function AddRoom(props) {
         })
     }
 
+    useEffect(() => {
+        if(findUnfinished() === -1){
+            if(!showSubmit) setShowSubmit(true)
+        }else{
+            if(showSubmit) setShowSubmit(false)
+        }
+    }, [steps])
+
+    const handleSubmit = () => {
+        
+    }
   return (
     <Container sx={{my:4}}>
         <Stepper alternativeLabel nonLinear activeStep={activeStep} sx={{mb:3}}>
@@ -77,13 +91,13 @@ function AddRoom(props) {
                 </Step>
             ))}
         </Stepper>
-        <Box>
+        <Box sx={{pb:7}}>
             {{
                 0:<AddLocation />,
                 1:<AddDetails />,
                 2:<AddImages />
             }[activeStep]}
-        </Box>
+
         <Stack direction='row' sx={{pt:2, pb:7, justifyContent:'space-around'}}>
             <Button color='inherit' disabled={!activeStep} onClick={()=>setActiveStep((activeStep)=>activeStep-1)}>
                 Back
@@ -92,6 +106,14 @@ function AddRoom(props) {
                 Next
             </Button>
         </Stack>
+        {showSubmit && (
+            <Stack sx={{alignItems:'center'}}>
+                <Button variant='contained' endIcon={<Send />} onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </Stack>
+        )}
+        </Box>
     </Container>
   )
 }
