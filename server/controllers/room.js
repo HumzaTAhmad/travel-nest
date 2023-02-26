1af4c572f0b170feb1cac751b7bbd78e33fe907a
HumzaTAhmad <humza3173@gmail.com>
2023-02-26 13:44:50 -0500

    Create CreateRoom Controller that will insert rooms into the roomModel collection in mongoDB.
    This controller will extract the information from the req.user that it is
    given through the authentication middleware that checks if the token is
    active. It will then save this room into the collection.

--- server/controllers/room.js
+++ server/controllers/room.js
@@ -1,7 +1,10 @@
+import roomModel from '../models/Room.js'
+import tryCatch from './utils/tryCatch.js'
 
 
-
-export async function createRoom(req, res) {
-    console.log("createRoom runs")
-    return res.status(201).json({success:true, result:{id:123, title:'test room'}});
-}
+export const createRoom = tryCatch(async (req, res) => {
+    const {id:uid, name:uname, photoURL:uPhoto} = req.user
+    const newRoom = new roomModel({...req.body, uid, uName, uPhoto})
+    await newRoom.save()
+    res.status(201).json({success:true, result:newRoom})
+})
