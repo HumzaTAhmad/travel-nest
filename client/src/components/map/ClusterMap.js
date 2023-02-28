0a4b270716d1adf11d713d5df7599c972cb85595
HumzaTAhmad <humza3173@gmail.com>
2023-02-28 14:24:16 -0500

    Call getRoom action from clustermap component. Implement mapbox in ClusterBox component
    This was done so we can display clusters of nearby rooms on the map. To
    do that first we must display a map on the maps page, which we are doing
    with this change.

--- client/src/components/map/ClusterMap.js
+++ client/src/components/map/ClusterMap.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {connect, useDispatch} from 'react-redux'
+import { getRooms } from '../../actions/rooms'
+import ReactMapGL from 'react-map-gl'
 
-export default function ClusterMap() {
+function ClusterMap(props) {
+  const rooms = props.rooms
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    getRooms(dispatch)
+  }, [])
+
+  useEffect(() => {
+    console.log(rooms)
+  }, [rooms])
+
   return (
-    <div>ClusterMap</div>
+    <ReactMapGL
+    initialViewState={{latitude:51.5072, longitude:0.1276}}
+    mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
+    mapStyle='mapbox://styles/mapbox/streets-v11'
+    >
+    </ReactMapGL>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    rooms: state.rooms
+  };
+}
+
+export default connect(mapStateToProps)(ClusterMap);
--- client/src/reducers/rooms.js
+++ client/src/reducers/rooms.js
@@ -1,6 +1,6 @@
 export default (state = [], action) => {
     switch (action.type){
-        case 'UPDATE_ROOMs':
+        case 'UPDATE_ROOMS':
             return [...state, action.payload];
         default:
             return state;
