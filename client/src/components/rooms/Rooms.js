9c95e0e920d60da547bf6b8135890f550f8d51fa
HumzaTAhmad <humza3173@gmail.com>
2023-03-04 12:43:12 -0500

    Create Rooms image list using map
    Created a rooms image list by mapping the rooms state array in our
    redux state in a card format in the rooms component tab. This will
    allow the user to view the list of all available rooms if they do not
    wish to view it on the global map.

--- client/src/components/rooms/Rooms.js
+++ client/src/components/rooms/Rooms.js
@@ -1,7 +1,65 @@
 import React from 'react'
+import {Avatar, Card, Container, ImageList, ImageListItem, ImageListItemBar, Rating, Tooltip} from '@mui/material'
+import { useDispatch, connect } from 'react-redux';
+import { StarBorder } from '@mui/icons-material';
 
-export default function Rooms() {
+function Rooms(props) {
+
+  const dispatch = useDispatch();
+  const rooms = props.rooms
+  console.log(rooms)
+
   return (
-    <div>Rooms</div>
-  )
+    <Container>
+      <ImageList gap={12} sx={{mb:8}}>
+        {
+          rooms.map(room=>(
+            <Card key={room._id}>
+              <ImageListItem sx={{height:'100% !important'}}>
+                <ImageListItemBar
+                  sx={{
+                    background:
+                      'linear-gradient(to bottom, rgba(0,0,0,0.7)0%, rgba(0,0,0,0.3)70%, rgba(0,0,0,0) 100%)',
+                  }}
+                  title={room.price===0 ? 'Free Stay' : '$' + room.price}
+                  actionIcon={
+                    <Tooltip title={room.uName} sx={{mr:'5px'}}>
+                      <Avatar src={room.uPhoto} />
+                    </Tooltip>
+                  }
+                  position='top'
+                />
+                <img src={room.images[0]} alt={room.title} loading='lazy' style={{cursor:'pointer'}}/>
+                <ImageListItemBar
+                title={room.title}
+                actionIcon={
+                  <Rating
+                  sx={{color:'rgba(255,255,255,0.8)', mr:'5px'}}
+                  name='room-rating'
+                  defaultValue={3.5}
+                  precision={0.5}
+                  emptyIcon={
+                    <StarBorder sx={{color:'rgba(255,255,255,0.8)'}}/>
+                  }
+                  />
+                }
+
+                />
+              </ImageListItem>
+            </Card>
+          ))
+        }
+      </ImageList>
+    </Container>
+  );
 }
+
+function mapStateToProps(state) {
+  console.log(state)
+  return {
+    rooms: state.rooms
+  };
+}
+
+export default connect(mapStateToProps)(Rooms);
+
--- client/src/reducers/rooms.js
+++ client/src/reducers/rooms.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
     switch (action.type){
         case 'UPDATE_ROOMS':
-            return [...state, action.payload];
+            return [...state, ...action.payload];
         default:
             return state;
     }
