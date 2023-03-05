e606aadd6a3560807cf0c9c972c25a836536f081
HumzaTAhmad <humza3173@gmail.com>
2023-03-05 18:12:13 -0500

    Launch room details when room is created by user
    This will allow the user to view the details of there newly created
    room without having to dig for it.

--- client/src/actions/rooms.js
+++ client/src/actions/rooms.js
@@ -11,6 +11,9 @@ export const createRoom = async (room, currentUser, dispatch, setPage) => {
         dispatch({type:'UPDATE_ALERT', payload:{open:true, severity:'success', message:'The room has been added successfully'}})
         dispatch({type:'RESET_ROOM'})
         setPage(0)
+
+        //launch room deatil in popup for user view when room is created
+        dispatch({type:'UPDATE_ROOM', payload:result}) //update room is the single room information that is launched with a popup when not null
     }
 
     dispatch({type:'END_LOADING'})
