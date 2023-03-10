fe0af2429d33f3ae3934f8057c3aa15f2af04ebe
HumzaTAhmad <humza3173@gmail.com>
2023-03-10 18:30:24 -0500

    Add user information in the side list of dashboard and logout functionality in dashboard
    User informaiton was added to the dashboard to allow users to verify themselves
    and there information.

--- client/src/pages/dashboard/SideList.js
+++ client/src/pages/dashboard/SideList.js
@@ -1,6 +1,8 @@
 import {ChevronLeft,Dashboard,Inbox,KingBed,Logout,Mail,MarkChatUnread, NotificationsActive,PeopleAlt,} from '@mui/icons-material';
 import {Avatar,Box,Divider,IconButton,List,ListItem,ListItemButton,ListItemIcon,ListItemText,styled,Tooltip,Typography,} from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer';
+import { connect, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -53,9 +55,15 @@ const openedMixin = (theme) => ({
   );
 
 
-export default function SideList({open, setOpen}) {
+function SideList({open, setOpen, currentUser}) {
 
-  
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch({type:'UPDATE_USER', payload:null})
+    navigate('/')
+  }
 
   return (
     <>
@@ -91,61 +99,37 @@ export default function SideList({open, setOpen}) {
           ))}
         </List>
         <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <Inbox /> : <Mail />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+        <Box sx={{ mx: 'auto', mt: 3, mb: 1 }}>
+          <Tooltip title={currentUser?.name || ''}>
+            <Avatar
+              src={currentUser?.photoURL}
+              {...(open && { sx: { width: 100, height: 100 } })}
+            />
+          </Tooltip>
+        </Box>
+        <Box sx={{ textAlign: 'center' }}>
+          {open && <Typography>{currentUser?.name}</Typography>}
+          <Typography variant="body2">{currentUser?.role || 'role'}</Typography>
+          {open && (
+            <Typography variant="body2">{currentUser?.email}</Typography>
+          )}
+          <Tooltip title="Logout" sx={{ mt: 1 }}>
+            <IconButton onClick={handleLogout}>
+              <Logout />
+            </IconButton>
+          </Tooltip>
+        </Box>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+
       </Box>
     </>
   )
 }
+
+const mapStateToProps = state => ({
+  currentUser: state.currentUser,
+});
+
+export default connect(mapStateToProps)(SideList);
