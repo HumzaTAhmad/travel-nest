e776c5053808a6ed92fb4dc440e0deafbcf425a3
HumzaTAhmad <humza3173@gmail.com>
2023-03-12 17:53:19 -0400

    Implement users datagrid in dasboard and pagination values of 5, 10, 20

--- client/src/pages/dashboard/users/Users.js
+++ client/src/pages/dashboard/users/Users.js
@@ -1,51 +1,103 @@
-import { Typography } from '@mui/material'
+import { Avatar, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import React, { useEffect, useMemo } from 'react'
-import {DataGrid} from '@mui/x-data-grid'
+import React, { useEffect, useMemo, useState } from 'react'
+import {DataGrid, gridClasses} from '@mui/x-data-grid'
 import { getUsers } from '../../../actions/user'
 import { connect, useDispatch } from 'react-redux'
+import moment from 'moment'
+import { grey } from '@mui/material/colors'
 
 function Users({setSelectedLink, link, users}) {
 
-    const dispatch = useDispatch()
+const dispatch = useDispatch()
 
-    useEffect(()=>{
-        setSelectedLink(link)
-        getUsers(dispatch)
-    }, [])
+  const [pageSize, setPageSize] = useState(5);
+  const [rowId, setRowId] = useState(null);
 
-    const columns = useMemo(()=>[
-        {field:'photoURL', headerName:'Avatar', width:60},
-        {field:'name', headerName:'Name', width:170},
-        {field:'email', headerName:'Email', width:200},
-        {field:'role', headerName:'Role', width:100},
-        {field:'active', headerName:'Active', width:100},
-        {field:'createdAt', headerName:'Created At', width:200},
-        {field:'_id', headerName:'Id', width:220}
-    ], [])
+  useEffect(() => {
+    setSelectedLink(link);
+    if (users.length === 0) getUsers(dispatch);
+  }, []);
 
-    return (
-        <Box
+  const columns = useMemo(
+    () => [
+      {
+        field: 'photoURL',
+        headerName: 'Avatar',
+        width: 60,
+        renderCell: (params) => <Avatar src={params.row.photoURL} />,
+        sortable: false,
+        filterable: false,
+      },
+      { field: 'name', headerName: 'Name', width: 170 },
+      { field: 'email', headerName: 'Email', width: 200 },
+      {
+        field: 'role',
+        headerName: 'Role',
+        width: 100,
+        type: 'singleSelect',
+        valueOptions: ['basic', 'editor', 'admin'],
+        editable: true,
+      },
+      {
+        field: 'active',
+        headerName: 'Active',
+        width: 100,
+        type: 'boolean',
+        editable: true,
+      },
+      {
+        field: 'createdAt',
+        headerName: 'Created At',
+        width: 200,
+        renderCell: (params) =>
+          moment(params.row.createdAt).format('YYYY-MM-DD HH:MM:SS'),
+      },
+      { field: '_id', headerName: 'Id', width: 220 },
+      {
+        field: 'actions',
+        headerName: 'Actions',
+      },
+    ],
+    [rowId]
+  );
+
+  return (
+    <Box
+      sx={{
+        height: 400,
+        width: '100%',
+      }}
+    >
+      <Typography
+        variant="h3"
+        component="h3"
+        sx={{ textAlign: 'center', mt: 3, mb: 3 }}
+      >
+        Manage Users
+      </Typography>
+      <DataGrid
+        columns={columns}
+        rows={users}
+        getRowId={(row) => row._id}
+        rowsPerPageOptions={[5, 10, 20]}
+        pageSize={pageSize}
+        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+        getRowSpacing={(params) => ({
+          top: params.isFirstVisible ? 0 : 5,
+          bottom: params.isLastVisible ? 0 : 5,
+        })}
         sx={{
-            height:400,
-            width:'100%'
+          [`& .${gridClasses.row}`]: {
+            bgcolor: (theme) =>
+              theme.palette.mode === 'light' ? grey[200] : grey[900],
+          },
         }}
-        >
-            <Typography
-            variant='h3'
-            component='h3'
-            sx={{textAlign:'center', mt:3, mb:3}}
-            >
-                Manage Users
-            </Typography>
-            <DataGrid 
-            columns={columns}
-            rows={users}
-            getRowId={row=>row._id}
-            />
-        </Box>
-    )
-}
+        onCellEditCommit={(params) => setRowId(params.id)}
+      />
+    </Box>
+  );
+};
 
 function mapStateToProps(state) {
     console.log(state)
